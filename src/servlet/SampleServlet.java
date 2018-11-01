39b9e4ee88e0c6ea20aa78f4c1ba154c6f4c20e4
toriitakashi <toriitakashi0712@gmail.com>
2018-11-01 23:37:43 +0900

    Update SampleServlet.java

--- src/servlet/SampleServlet.java
+++ src/servlet/SampleServlet.java
@@ -37,7 +37,7 @@ public class SampleServlet extends HttpServlet {
 
 	    TestBean bean = new TestBean();
 	    bean.setFirstName("1234567890");
-	    bean.setEmail("test@dialonet");
+	    bean.setEmail("test@test");
 
 	    Cleaning c = new Cleaning(bean);
 	    try {
